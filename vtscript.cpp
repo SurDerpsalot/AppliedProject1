3c5e70873cb317957ec8ea33f28bf5c60a207ef3
Brad <bfin96@vt.edu>
2017-03-06 21:58:37 -0500

    Am now passing all 8 tests and have a very small memory leak. WOOOOOO HOOOOOOOO!!!!! This is a great day!

--- vtscript.cpp
+++ vtscript.cpp
@@ -7,7 +7,7 @@ using std::cin;
 
 int main(int argc, char*argv[])
 {
-	int output;
+	int Output;
 	Expression Error("Error");
 	if (argc == 1) 
 	{
@@ -36,7 +36,7 @@ int main(int argc, char*argv[])
 				cout << "Error" << std::endl;
 				Interp.resetEnviro();
 			}
-			output = EXIT_SUCCESS;
+			Output = EXIT_SUCCESS;
 		}
 	}
 	else if (argc == 3)
@@ -44,7 +44,6 @@ int main(int argc, char*argv[])
 		std::string input = argv[2];
 		Interpreter Interp;
 		Expression output;
-		std::cout << input << std::endl;
 		std::istringstream TokenStream(input);
 		Interp.parse(TokenStream);
 		try {
@@ -52,23 +51,23 @@ int main(int argc, char*argv[])
 		}
 		catch (InterpreterSemanticError & ERR)
 		{
-			throw ERR;
-			output = EXIT_SUCCESS;
+			cout << "Error" << std::endl;
+			Output = EXIT_FAILURE;
+			return Output;
 		}
 		if (output.Node.type == Bool)
 			cout << "(" << output.Node.bool_value << ")" << std::endl;
 		else if (output.Node.type == Value)
 			cout << "(" << output.Node.double_value << ")" << std::endl;
-		cout << "end" << std::endl;
-		output = EXIT_SUCCESS;
+		Output = EXIT_SUCCESS;
 	}
 	else if(argc == 2)
 	{
 		std::ifstream in(argv[1]);
 		if (!in.is_open()) 
 		{
-			cout << "The input file cout not be opened" << std::endl;
-			output = EXIT_FAILURE;
+			cout << "Error" << std::endl;
+			Output = EXIT_FAILURE;
 		}
 		else
 		{
@@ -80,16 +79,16 @@ int main(int argc, char*argv[])
 			}
 			catch (InterpreterSemanticError & ERR)
 			{
-				throw ERR;
-				output = EXIT_SUCCESS;
+				cout << "Error" << std::endl;
+				Output = EXIT_FAILURE;
 			}
 			if (output.Node.type == Bool)
 				cout << "(" << output.Node.bool_value << ")" << std::endl;
 			else if (output.Node.type == Value)
 				cout << "(" << output.Node.double_value << ")" << std::endl;
+			Output = EXIT_SUCCESS;
 			in.close();
-			output = EXIT_SUCCESS;
 		}
 	}
-	return output;
+	return Output;
 }
